efd2d1411e480ccf75dd426299ab9b449db3e866
lemonade455 <161819133+Lemonade455@users.noreply.github.com>
2025-12-03 08:06:54 +0100

    Update index route title in app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "<h1>diyHue Pair Assistant – Emulator</h1>"
+    return "<h1>Lemonade455-diyhue – Emulator</h1>"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
